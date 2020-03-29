59a60106d5d9a9419949f5a64eb39f3196e8d232
Yaping Yang <yapingyang94@gmail.com>
2020-03-29 17:34:02 -0400

    first commit

---
+++ vue-weather-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: '74038c44a26d23ab623f096fcb9bec45'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
